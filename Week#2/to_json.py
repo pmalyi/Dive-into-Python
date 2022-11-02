0fc92f6b7d4c25ae07efd5bebd189904e7a6812c
pmalyi <pmalyi@gmail.com>
2022-11-02 10:49:41 +0200

    Decorator to_json

--- Week#2/to_json.py
+++ Week#2/to_json.py
@@ -1,10 +1,13 @@
+# Декоратор to_json
 import functools
+import json
 
 
 def to_json(func):
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
         result = func(*args, **kwargs)
+        result = json.dumps(result)
         print(result)
         return result
     return wrapped
